5243aa10ae760c09a7b2b9db2c266c3f90b576f4
Yugyeong-Ji <2monica9@naver.com>
2022-07-19 10:32:51 +0900

    Back to the Merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:baljachwi_project/screens/catalog_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
+
 import 'package:baljachwi_project/screens/mypage/mypage_screen.dart';
+import 'package:baljachwi_project/screens/nonmember_home_screen.dart';
+import 'package:baljachwi_project/screens/catalog_list_screen.dart';
+import 'package:baljachwi_project/screens/nonmember_order_screen.dart';
+import 'package:baljachwi_project/screens/product_details_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
