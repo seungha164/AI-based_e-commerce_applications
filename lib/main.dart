f0279ea88ec4c92f807e64cee73f49af517b9a83
nute11a <87134443+seungha164@users.noreply.github.com>
2022-07-20 04:02:56 +0900

    :card_file_box: firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:baljachwi_project/screens/catalog_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
-import 'package:baljachwi_project/screens/mypage/mypage_screen.dart';
-import 'package:baljachwi_project/screens/nonmember_home_screen.dart';
-import 'package:baljachwi_project/screens/catalog_list_screen.dart';
-import 'package:baljachwi_project/screens/product_details_screen.dart';
 import 'package:baljachwi_project/screens/Home/home.dart';
-
-void main() {
+// firebase core 플러그인 및 생성한 구서파일 가져오기
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -19,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //debugShowCheckedModeBanner: false,
-      home: mypage(),
+      home: Home(),
     );
   }
 }
